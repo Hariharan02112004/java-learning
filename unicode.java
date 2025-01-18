a6995ef464c42fabd25d9d0c6330f01f515a5b99
Hariharan G <167274701+Hariharan02112004@users.noreply.github.com>
2025-01-18 10:21:31 +0530

    Add files via upload

---
+++ unicode.java
@@ -0,0 +1,12 @@
+public class unicode{
+    public static void main(String[]args){
+        char mychar='\u00A9';
+        System.out.println(mychar);
+        char mychar1='\u00A1';
+        System.out.println(mychar1);
+        char mychar2='\u00AE';
+        System.out.println(mychar2);
+        char mychar3='\u00A3';
+        System.out.println(mychar3);
+    }
+}
