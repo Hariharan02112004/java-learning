4a3eb4e32fe7b42b437be56f895a4515fcab2b6a
Hariharan G <167274701+Hariharan02112004@users.noreply.github.com>
2025-01-21 22:01:20 +0530

    Add files via upload

---
+++ max_min_value.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class max_min_value{
+    public static void main(String[]args){
+        int i1= Integer.MAX_VALUE;
+        int i2= Integer.MIN_VALUE;
+        short s1=Short.MAX_VALUE;
+        short s2=Short.MIN_VALUE;
+        byte b1=Byte.MAX_VALUE;
+        byte b2=Byte.MIN_VALUE;
+        long l1=Long.MAX_VALUE;
+        long l2=Long.MIN_VALUE;
+        System.out.println("max int "+i1);
+        System.out.println("min int "+i2);
+        System.out.println("max short "+s1);
+        System.out.println("min short "+s2);
+        System.out.println("max byte "+b1);
+        System.out.println("min byte "+b2);
+        System.out.println("max long "+l1);
+        System.out.println("min long "+l2);
+        
+    }
+}
