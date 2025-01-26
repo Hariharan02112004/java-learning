5fb5b49150e92c512e1229b16a84aa99dfe18c4a
Hariharan G <167274701+Hariharan02112004@users.noreply.github.com>
2025-01-26 11:52:47 +0530

    Add files via upload

---
+++ Str_normalize.java
@@ -0,0 +1,22 @@
+//Normalize String
+//three ways to normalize a string
+// 1. use replaceAll("\\s", "") method to replace spaces at first, last and inbetween letter
+// 2. use trim() method to replace spaces at string beginning and ending
+// 4. use trim().replaceAll("\\s+","") method to remove extra places inbetween the sentence 
+// 3. use replaceAll("\\s", "") to replace spaces only at 
+import java.util.Scanner;
+public class Str_normalize{
+	public static String normal_str(String str){
+		str=str.replaceAll("\\s+","");
+		return str;
+	}
+	public static void main(String[]args){
+		Scanner in=new Scanner(System.in);
+		System.out.print("Enter a String: ");
+		String str=in.nextLine();
+		String result = normal_str(str);
+		System.out.print("The normalized string is "+result);
+		in.close();
+
+	}
+}
