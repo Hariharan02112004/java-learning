6bc0feb8d153a76b5fccb6f9608900c34ab3a51e
Hariharan G <167274701+Hariharan02112004@users.noreply.github.com>
2025-01-29 23:24:25 +0530

    Update mat_add.java

--- mat_add.java
+++ mat_add.java
@@ -9,14 +9,29 @@ public class mat_add{
 		int row=input.nextInt();
 		System.out.print("Enter number of colums: ");
 		int cols=input.nextInt();
-		System.out.println("Matrix Addition");
+		System.out.println("ENTER FIRST MATRIX: ");
 		int[][] mat1=new int[row][cols];
 		for(int i=0;i<row;i++){
 			for(int j=0;j<cols;j++){
 				mat1[i][j]=input.nextInt();
 			}
 		}
-		printMatix(mat1);
+		System.out.println("ENTER SECOND MATRIX: ");
+		int[][] mat2=new int[row][cols];
+		for(int i=0;i<row;i++){
+			for(int j=0;j<cols;j++){
+				mat2[i][j]=input.nextInt();
+			}
+		}
+		int result[][]=new int[row][cols];
+		System.out.println("Matrix Addition");
+		for(int i=0;i<row;i++){
+			for(int j=0;j<cols;j++){
+				result[i][j]=mat1[i][j]+mat2[i][j];
+				System.out.print(result[i][j]);
+			}
+			System.out.println();
+		}
 
 	}
 }
